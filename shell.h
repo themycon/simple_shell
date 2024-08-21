5d4bdc72fd3d62327986f44bda9516b692afb3d5
themycon <mturyasingura97@gmail.com>
2024-08-21 14:14:17 -0700

    main.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+extern char **environ;  /* Declaration of the environ variable */
+
+void handle_eof(void);
+ssize_t read_input(char **input_line, size_t *buffer_size);
+void execute_command(char *command);
+
+#endif /* SHELL_H */
